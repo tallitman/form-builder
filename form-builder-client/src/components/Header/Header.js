d5a3837212fb7385d21d526f10941668d3f40292
Marik Sh <mark22312@gmail.com>
2019-07-14 13:24:06 +0300

    Fixed Header

--- form-builder-client/src/components/Header/Header.js
+++ form-builder-client/src/components/Header/Header.js
@@ -14,7 +14,15 @@ export default class Header extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			isOpen: false
+		};
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState({isOpen: !this.state.isOpen});
 	}
 
 	render() {
